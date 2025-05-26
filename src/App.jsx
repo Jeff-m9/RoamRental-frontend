55f7905ca2b1506389c1f1fd3c11b26a44f26298
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-05-26 17:12:06 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from './components/Header'
+import { SearchBar } from './components/Search-bar'
+import { Filter } from './components/FIlter'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <SearchBar />
+      <Filter />
+    </>
+  )
+}
+
+export default App
