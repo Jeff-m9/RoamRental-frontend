d992cb5a408f7297e6bcf2c7b7e306a2faa837fb
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-05-26 17:15:49 +0300

    added filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,13 @@
+export function Filter() {
+  return (
+    <div>
+      <div>
+        <select name="Categories">
+          <option value="">All</option>
+          <option value="SUV">SUV</option>
+          <option value="sedan">Sedan</option>
+        </select>
+      </div>
+    </div>
+  );
+}
